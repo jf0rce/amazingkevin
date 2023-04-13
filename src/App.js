70bb2fdc2d477354e8ee35409eca6c0d07b453f1
Joe Forsythe <jforsythe@truefit.io>
2023-04-13 16:21:02 -0400

    add alt

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import Marquee from 'react-fast-marquee';
 function App() {
   return (
     <Marquee style={{height: '100vh'}}>
-      <img src={require('./assets/kevin1.jpg')} style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-      <img src={require('./assets/kevin2.jpg')} style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-      <img src={require('./assets/kevin3.jpg')} style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-      <img src={require('./assets/kevin4.jpg')} style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-      <img src={require('./assets/kevin5.jpg')} style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={require('./assets/kevin1.jpg')} alt="1" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={require('./assets/kevin2.jpg')} alt="2" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={require('./assets/kevin3.jpg')} alt="3" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={require('./assets/kevin4.jpg')} alt="4" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={require('./assets/kevin5.jpg')} alt="5" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
     </Marquee>
   );
 }

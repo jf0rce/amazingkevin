f0e6795e3743f0e214399bc8d5310b1c73725b29
Joe Forsythe <jforsythe@truefit.io>
2023-04-13 15:04:13 -0400

    adjust sizes and get marquee scrolling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import kevin1 from './assets/kevin1.jpg';
+import kevin2 from './assets/kevin2.jpg';
+import kevin3 from './assets/kevin3.jpg';
+import kevin4 from './assets/kevin4.jpg';
+import kevin5 from './assets/kevin5.jpg';
+import Marquee from 'react-fast-marquee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Marquee style={{height: '100vh'}}>
+      <img src={kevin1} style={{height: '80vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={kevin2} style={{height: '80vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={kevin3} style={{height: '80vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={kevin4} style={{height: '80vh', marginLeft: 30, marginRight: 30}}/>
+      <img src={kevin5} style={{height: '80vh', marginLeft: 30, marginRight: 30}}/>
+    </Marquee>
   );
 }
 

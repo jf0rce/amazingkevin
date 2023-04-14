80645afd6e3738ffa945c9de4bf9d30ce681ae46
Joe Forsythe <jforsythe@truefit.io>
2023-04-14 09:41:36 -0400

    remove marquee library and diy

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import kevin2 from './assets/kevin2.jpg';
 import kevin3 from './assets/kevin3.jpg';
 import kevin4 from './assets/kevin4.jpg';
 import kevin5 from './assets/kevin5.jpg';
-import Marquee from 'react-fast-marquee';
+import { Banner } from './Banner';
 
 function App() {
+  const images = [
+    kevin1,
+    kevin2,
+    kevin3,
+    kevin4,
+    kevin5
+  ].map((image) => ({
+    id: crypto.randomUUID(),
+    image
+  }));
+
   return (
-    <Marquee style={{height: '100vh'}}>
-      <img src={require('./assets/kevin1.jpg')} alt="1" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-      <img src={require('./assets/kevin2.jpg')} alt="2" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-      <img src={require('./assets/kevin3.jpg')} alt="3" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-      <img src={require('./assets/kevin4.jpg')} alt="4" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-      <img src={require('./assets/kevin5.jpg')} alt="5" style={{height: '50vh', marginLeft: 30, marginRight: 30}}/>
-    </Marquee>
+    <div className='App'>
+      <Banner images={images} speed={25500} />
+    </div>
   );
 }
 
